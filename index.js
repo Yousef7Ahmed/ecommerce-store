836a7ee7ad2a3aa913f3963f70ebf669d79a13db
Your Name <you@example.com>
2026-07-12 15:18:51 +0300

    Connect MongoDB Atlas

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const mongoose = require("mongoose");
-
+console.log("MONGO_URI =", process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI);
 
 // Middleware
